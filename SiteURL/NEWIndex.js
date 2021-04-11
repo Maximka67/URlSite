60237a913f85d5a8f8c8507f21fa0b4a07aca056
Maximka67 <65401533+Maximka67@users.noreply.github.com>
2021-04-11 22:51:35 +0500

    Add files via upload

--- SiteURL/NEWIndex.js
+++ SiteURL/NEWIndex.js
@@ -3,11 +3,22 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
-const {Client} = require('pg');
-const url = require('url');
-const http = require('http')
-
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+
+const Hashids = require('hashids/cjs')
+const {Client} = require('pg');
+
+const db = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'URL',
+    password: '123',
+    port: 5432  
+});
+
+db.connect();
+
 
 app.set('view engine','ejs');
 
@@ -26,123 +37,94 @@ app.get('/shortURL', function(req,res){
 app.post('/shortURL', urlencodedParser, function(req, response1){
 
     var res = '';
-    const {Client} = require('pg');
-
-    const db = new Client({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'URL',
-        password: '123',
-        port: 5432  
-    });
-    
-    db.connect();
     var text = req.body.fURL;
     
-    db.query('SELECT * FROM shorturl', (err, data) => {
+    db.query('SELECT * FROM shorturl', async (err, data) => {
+
+        var flag = false;
+
+
+
         if (err) 
             throw new Error(err);
-        
-        console.log("sdsad");
                     
-        checkKey();
-    
         
-        
-        function checkKey() {
-            var flag = false;
 
-            for(var i = 0; i < data.rows.length; i++) {
-                if('FullUrl' in data.rows[i]) {
-                    if (data.rows[i]['FullUrl'] == text){
-                        flag = true;
-                        res = data.rows[i]['ShortUrl'];
-                        console.log(data.rows[i]['ShortUrl']);
-                        send();
-                    }
-                }   
-            }
-
-            if (!flag){
-                createShortUrl()
-            } else {
-                db.end();
-            }
+        for(var i = 0; i < data.rows.length; i++) {
+            if('FullUrl' in data.rows[i]) {
+                if (data.rows[i]['FullUrl'] == text){
+                    flag = true;
+                    res = data.rows[i]['ShortUrl'];
+                    console.log(data.rows[i]['ShortUrl']);
+                    
+                    await send();
+                    
+                }
+            }   
         }
-        
-    
-        function createShortUrl(){
 
-            const Hashids = require('hashids/cjs')
-            const hashids = new Hashids(text);
+        if (!flag){
+            // console.log('Вызов функции');
+            // console.log('Начало выполнения');
+            
+            const hashids = new Hashids(text,5);
             res = hashids.encode(1); 
-            
-            db.query("INSERT INTO shorturl VALUES ($1, $2)",[text, res], function(err, result){
+    
+            await db.query("INSERT INTO shorturl VALUES ($1, $2)",[text, res]);
 
-                console.log(err);
-                db.end();
-                send();
-                
-            });
-            
+            await send();
+           
         }
-    
-    })
-    
-    function send() {
-        response1.render('shortURL', {
+        
+
+        async function send(){
+         
+            response1.render('shortURL', {
             data: res
         });
-        console.log("res = "+ res);
-    }
+         console.log("res = "+ res);
+        
+        } 
+    
+    })
+
+
+
+
+    
 });
 
 app.get('/:token', function(req,res){
 
     var tokens = req.params.token;
     var URLDone;
-    ////////////////////////////////////////////////////
-    const {Client} = require('pg');
-    
-        const db = new Client({
-            user: 'postgres',
-            host: 'localhost',
-            database: 'URL',
-            password: '123',
-            port: 5432  
-        });
         
-        db.connect();
-        
-        db.query('SELECT * FROM shorturl', (err, data) => {
-            if (err) 
-                throw new Error(err);
-                        
-            checkKey1();
+    db.query('SELECT * FROM shorturl', (err, data) => {
+        if (err) 
+            throw new Error(err);
             
-            function checkKey1() {
-                
-                var flag = false;
-                for(var i = 0; i < data.rows.length; i++) {
+        var flag = false;
+        for(var i = 0; i < data.rows.length; i++) {
+            
+            if('ShortUrl' in data.rows[i]) {
+            
+                if (data.rows[i]['ShortUrl'] == tokens){
                     
-                  if('ShortUrl' in data.rows[i]) {
+                    flag = true;
+                    console.log("Полная ссылка - " + data.rows[i]['FullUrl']);
+                    URLDone = data.rows[i]['FullUrl'];
+                    send(URLDone);
                     
-                    if (data.rows[i]['ShortUrl'] == tokens){
-                        
-                        flag = true;
-                        console.log("Полная ссылка - " + data.rows[i]['FullUrl']);
-                        URLDone = data.rows[i]['FullUrl'];
-                        send(URLDone);
-                        db.end();
-                    }
-                  }
                 }
-            }      
-            });
-            function send(url) {
-                res.redirect(301, url);
-                console.log("res = "+ url);
             }
+        }
+                
+    });
+
+    function send(url) {
+        res.redirect(301, url);
+        console.log("res = "+ url);
+    }
 });
         
 app.listen(3000);

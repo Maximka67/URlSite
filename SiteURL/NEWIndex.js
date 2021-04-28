e00f015d848bef47078bae6559d5f501abc8cfcc
Maximka67 <65401533+Maximka67@users.noreply.github.com>
2021-04-29 02:09:08 +0500

    Add files via upload

--- SiteURL/NEWIndex.js
+++ SiteURL/NEWIndex.js
@@ -1,11 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
-
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false});
 const Hashids = require('hashids/cjs')
 const {Client} = require('pg');
 
@@ -18,7 +14,6 @@ const db = new Client({
 });
 
 db.connect();
-
 
 app.set('view engine','ejs');
 
@@ -34,97 +29,58 @@ app.get('/shortURL', function(req,res){
     res.render('index');
 });
 
-app.post('/shortURL', urlencodedParser, function(req, response1){
-
+app.post('/shortURL', urlencodedParser, async function(req, response1){
     var res = '';
     var text = req.body.fURL;
+    var data = await db.query('SELECT * FROM shorturl');
+    var flag = false;
+                
+    for(var i = 0; i < data.rows.length; i++) {
+        if('FullUrl' in data.rows[i]) {
+            if (data.rows[i]['FullUrl'] == text){
+                flag = true;
+                res = data.rows[i]['ShortUrl'];
+                console.log(data.rows[i]['ShortUrl']);
+                await send();   
+            }
+        }   
+    }
+
+    if (!flag){            
+        const hashids = new Hashids(text,5);
+        res = hashids.encode(1);     
+        await db.query("INSERT INTO shorturl VALUES ($1, $2)",[text, res]);
+        await send();
+    }
     
-    db.query('SELECT * FROM shorturl', async (err, data) => {
+    async function send(){
+        response1.render('shortURL', {
+        data: res
+    });
+        console.log("token = "+ res);
+    } 
+    });
 
-        var flag = false;
-
-
-
-        if (err) 
-            throw new Error(err);
-                    
-        
-
-        for(var i = 0; i < data.rows.length; i++) {
-            if('FullUrl' in data.rows[i]) {
-                if (data.rows[i]['FullUrl'] == text){
-                    flag = true;
-                    res = data.rows[i]['ShortUrl'];
-                    console.log(data.rows[i]['ShortUrl']);
-                    
-                    await send();
-                    
-                }
-            }   
-        }
-
-        if (!flag){
-            // console.log('Вызов функции');
-            // console.log('Начало выполнения');
-            
-            const hashids = new Hashids(text,5);
-            res = hashids.encode(1); 
-    
-            await db.query("INSERT INTO shorturl VALUES ($1, $2)",[text, res]);
-
-            await send();
-           
-        }
-        
-
-        async function send(){
-         
-            response1.render('shortURL', {
-            data: res
-        });
-         console.log("res = "+ res);
-        
-        } 
-    
-    })
-
-
-
-
-    
-});
-
-app.get('/:token', function(req,res){
-
+app.get('/:token', async function(req,res){
     var tokens = req.params.token;
     var URLDone;
         
-    db.query('SELECT * FROM shorturl', (err, data) => {
-        if (err) 
-            throw new Error(err);
-            
-        var flag = false;
-        for(var i = 0; i < data.rows.length; i++) {
-            
-            if('ShortUrl' in data.rows[i]) {
-            
-                if (data.rows[i]['ShortUrl'] == tokens){
-                    
-                    flag = true;
-                    console.log("Полная ссылка - " + data.rows[i]['FullUrl']);
-                    URLDone = data.rows[i]['FullUrl'];
-                    send(URLDone);
-                    
-                }
+    var data = await db.query('SELECT * FROM shorturl'); 
+
+    var flag = false;
+    for(var i = 0; i < data.rows.length; i++) {
+        if('ShortUrl' in data.rows[i]) {
+            if (data.rows[i]['ShortUrl'] == tokens){
+                
+                flag = true;
+                console.log("Полная ссылка - " + data.rows[i]['FullUrl']);
+                URLDone = data.rows[i]['FullUrl'];
+                res.redirect(301, URLDone);
+                console.log("res = "+ URLDone);
             }
         }
-                
-    });
-
-    function send(url) {
-        res.redirect(301, url);
-        console.log("res = "+ url);
-    }
+    };
 });
         
 app.listen(3000);
+console.log('Запуск выполнен, порт 3000');
